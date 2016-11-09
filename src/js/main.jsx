fc2fd039dc178a33622d7849b09bf4dc8661aeb5
Matt Catoe <catoemw@gmail.com>
2016-11-09 12:21:54 -0500

    added react-router

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,18 @@
-function app () {
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
 
-}
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 
-app();
+var App = require('./components/App.jsx');
+
+var jsx = (
+	<Router history={hashHistory}>
+		<Route path="/" component={App}></Route>
+	</Router>
+);
+
+ReactDOM.render(jsx, document.querySelector('#app'));
