c31d1b56e87e19c8ce4c525ea8a75be0e58931db
Matt Catoe <catoemw@gmail.com>
2016-11-14 09:35:44 -0500

    added lowdb

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+var lowdb = require('lowdb');
+var fileAsync = require('lowdb/lib/file-async');
+var shortid = require('shortid');
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var db = lowdb('db.json', { storage: fileAsync });
 var app = express();
 var port = 3000;
+
+app.use(bodyParser());
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/lib'));
 app.use(express.static(__dirname + '/src/css'));
 
-
+db.defaults({
+	// any default collections (arrays)
+}).value();
 
 app.listen(port);
